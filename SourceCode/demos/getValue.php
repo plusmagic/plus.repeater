18e9dbbf8283ce10ca9d43cc2a1803af4af17b4a
Pitt Phunsanit <Phunsanit@gmail.com>
2025-02-24 22:58:29 +0700

    - update html - defined variable with let

--- SourceCode/demos/getValue.php
+++ SourceCode/demos/getValue.php
@@ -1,26 +1,28 @@
 <!doctype html>
-<html>
+<html lang="en">
+
 <head>
-<meta charset="utf-8">
-<title>plus.repeater :: get value</title>
+	<meta charset="utf-8">
+	<title>jQuery plus.repeater: get value</title>
 </head>
+
 <body>
-<table border="1" width="98%">
-	<thead>
-		<tr>
-			<th width="20">No</th>
-			<th>link</th>
-			<th>target</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php
-foreach($_POST['links'] as $no => $link)
-{
-	echo '<tr><td>',$no,'</td><td>',$link['href'],'</td><td>',$link['target'],'</td></tr>';
-}
-?>
-	</tbody>
-</table>
+	<table style="border: 1px solid black;width: 98%;">
+		<thead>
+			<tr>
+				<th style="width: 20px;">#no#</th>
+				<th>link</th>
+				<th>target</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			foreach ($_POST['links'] as $no => $link) {
+				echo '<tr><td>', $no, '</td><td>', $link['href'], '</td><td>', $link['target'], '</td></tr>';
+			}
+			?>
+		</tbody>
+	</table>
 </body>
+
 </html>
--- SourceCode/src/jQuery-plus-repeater.js
+++ SourceCode/src/jQuery-plus-repeater.js
@@ -1,6 +1,6 @@
 /*
 <!---=-=-=-=-=-=-=-[Plus Repeater]-=-=-=-=-=-=-=--->
-plus.repeater v.1.5.1
+jQuery plus.repeater v.1.5.2
 form repeater tools
 ====================================================
 Copyright (c) 2014 by Pitt Phunsanit
@@ -17,12 +17,12 @@ Back to the developer for the benefit of everyone.
 jQuery.repeater = function () {
 	var options = $.extend({
 		callback: function () { },
+		container: 'repeater',
 		deepWithDataAndEvents: 'default',
 		default: 0,
 		max: 10,
 		min: 1,
 		parent: 1,
-		selecter: 'repeater',
 		start: 0,
 		withDataAndEvents: false
 	}, arguments[0] || {});
@@ -32,9 +32,9 @@ jQuery.repeater = function () {
 	}
 
 	function range() {
-		var count = $('#' + options.selecter + ' .clone').length;
+		var count = $('#' + options.container + ' .clone').length;
 		if (count < (options.min + 1)) {
-			for (a = count; a <= options.min; a++) {
+			for (let a = count; a <= options.min; a++) {
 				repeat();
 			}
 		}
@@ -48,34 +48,34 @@ jQuery.repeater = function () {
 
 	function repeat() {
 		++options.start;
-		id = options.selecter + options.start;
-		html = $('#' + options.selecter + ' .clone:first').html().replace(/#no#/g, options.start).replace(/#id#/g, id);
-		$('#' + options.selecter + ' .clone:first').clone(options.withDataAndEvents, options.deepWithDataAndEvents).attr('id', id).html(html).insertAfter($('#' + options.selecter + ' .clone:last')).show();
+
+		let id = options.container + options.start;
+		let html = $('#' + options.container + ' .clone:first').html().replace(/#no#/g, options.start).replace(/#id#/g, id);
+		$('#' + options.container + ' .clone:first').clone(options.withDataAndEvents, options.deepWithDataAndEvents).attr('id', id).html(html).insertAfter($('#' + options.container + ' .clone:last')).show();
 	}
 
 	this.status = function () {
-		parent = $('#' + options.selecter + ' .clone').parentsUntil(' .clone').attr('id').remove();
+		parent = $('#' + options.container + ' .clone').parentsUntil(' .clone').attr('id').remove();
 	}
 
 	if (options.default != 0) {
-		var count = $('#' + options.selecter + ' .clone').length;
+		var count = $('#' + options.container + ' .clone').length;
 		for (a = count; a <= options.default; a++) {
 			repeat();
 		}
-	}
-	else {
+	} else {
 		range();
 	}
-	$('#' + options.selecter + ' .clone:first').hide();
+	$('#' + options.container + ' .clone:first').hide();
 
-	$('#' + options.selecter).on('click', '.clone:last', function () {
+	$('#' + options.container).on('click', '.clone:last', function () {
 		range();
 	});
 
-	$('#' + options.selecter).on('click', '.remove', function (event) {
+	$('#' + options.container).on('click', '.remove', function (event) {
 		parent = $(this).parents('.clone').remove();
 	});
 
 	options.callback.call(this);
-	return $('#' + options.selecter);
+	return $('#' + options.container);
 }
